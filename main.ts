4b822f6b38549b3e81f5c43ef861a39972e7db6b
chaya2z <taisuke.nakano@chaya2z.com>
2024-05-13 15:58:01 +0900

    Add console authenticated user

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
-const main = () => {
-  console.log(Deno.env.get("GITHUB_TOKEN"))
+import { Octokit } from "https://esm.sh/octokit?dts";
+
+const main = async () => {
+  // authentication
+  const octokit = new Octokit({
+    auth: Deno.env.get("GITHUB_TOKEN"),
+  });
+
+  // get user
+  const { data: { login } } = await octokit.rest.users.getAuthenticated();
+
+  return login;
 };
 
 if (import.meta.main) {
-  main();
+  const user  = await main();
+  console.log(user);
+
+  Deno.exit();
 }
